e3a1c7a2592adb9d75375615b2a3cc2c6de9f8c6
areg98 <areg98>
2022-10-17 23:30:05 +0400

    added remove() method

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -3,8 +3,6 @@
 // That is the largest possible value that can be accessed in an ArrayList
 // Our Array list max size is 200000000, because of:
 // 1. Requested array size exceeds VM limit
-//
-//
 // 2. Java heap space
 
 public class MyArrayList {
@@ -28,6 +26,21 @@ public class MyArrayList {
         return true;
     }
 
+    public void remove(int index){
+        this.arr = new int[--size];
+        for (int i = 0, j = 0; i < size; i++, j++) {
+            if (i == index) {
+                j++;
+            }
+            arr[i] = arr1[j];
+        }
+        this.arr1 = new int[fullSize];
+        this.temp--;
+        for (int i = 0; i < size; i++) {
+            arr1[i] = arr[i];
+        }
+    }
+
     public int get(int index) {
         return arr[index];
     }
@@ -35,21 +48,24 @@ public class MyArrayList {
     public int size() {
         return this.size;
     }
-    public boolean contains(int item){
+
+    public boolean contains(int item) {
         for (int i = 0; i < this.size; i++) {
             if (arr[i] == item) return true;
 
         }
         return false;
     }
-    public void clear (){
+
+    public void clear() {
         size = 0;
         temp = -1;
         this.arr = new int[size];
         this.arr1 = new int[fullSize];
 
     }
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return (size == 0);
     }
 }
--- src/TestMyArrayList.java
+++ src/TestMyArrayList.java
@@ -4,8 +4,7 @@ import java.util.List;
 public class TestMyArrayList {
     public static void main(String[] args) {
         MyArrayList list = new MyArrayList();
-        MyArrayList list1 = new MyArrayList();
-        for (int i = 1; i < 26; i++) {
+        for (int i = 1; i <= 10; i++) {
             list.add(i);
 
         }
@@ -13,15 +12,19 @@ public class TestMyArrayList {
             System.out.print(list.get(i) + " ");
 
         }
-        System.out.println();
-        list.clone(list1);
-        List<Integer>arr = new ArrayList<Integer>() ;
-        arr.add(4);
-        System.out.println(arr.get(0));
 
-        MyArrayList list1 = new MyArrayList();
-        list1.add(4);
-        System.out.println(list1.get(0));
+        list.remove(1);
+        System.out.println();
+        list.add(5);
+        list.remove(1);
+        list.add(6);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+
+        }
+
+
+
 
     }
 }

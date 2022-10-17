33fe8abda304fc878bc6afaddc2843af74e0ffbc
areg98 <areg98>
2022-10-17 20:22:38 +0400

    refactor

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -3,36 +3,35 @@
 // That is the largest possible value that can be accessed in an ArrayList
 // Our Array list max size is 200000000, because of:
 // 1. Requested array size exceeds VM limit
+//
+//
 // 2. Java heap space
 
 public class MyArrayList {
-        private final int fullSize = 200000000;
-        private int size;
-        private int temp;
-        private int[] intArr, intArr1;
+    private final int fullSize = 200000000;
+    private int size;
+    private int temp;
+    private int[] intArr, intArr1;
 
-        MyArrayList() {
+    MyArrayList() {
+        temp = -1;
+        this.intArr1 = new int[fullSize];
+    }
 
-
-            temp = -1;
-            this.intArr1 = new int[fullSize];
-
-
+    public boolean add(int item) {
+        this.intArr = new int[++size];
+        intArr1[++temp] = item;
+        for (int i = 0; i < size; i++) {
+            intArr[i] = intArr1[i];
         }
-        public boolean add(int item) {
-            this.intArr = new int[++size];
-            intArr1[++temp] = item;
-            for (int i = 0; i < size; i++) {
-                intArr[i] = intArr1[i];
-            }
-            return true;
-        }
+        return true;
+    }
 
-        public int get(int index) {
-           return intArr[index];
-        }
+    public int get(int index) {
+        return intArr[index];
+    }
 
-        public int size(){
-            return this.size;
-        }
+    public int size() {
+        return this.size;
+    }
 }
--- src/TestMyArrayList.java
+++ src/TestMyArrayList.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestMyArrayList {
     public static void main(String[] args) {
         MyArrayList list = new MyArrayList();
+        MyArrayList list1 = new MyArrayList();
         for (int i = 1; i < 26; i++) {
             list.add(i);
 
@@ -9,5 +13,15 @@ public class TestMyArrayList {
             System.out.print(list.get(i) + " ");
 
         }
+        System.out.println();
+        list.clone(list1);
+        List<Integer>arr = new ArrayList<Integer>() ;
+        arr.add(4);
+        System.out.println(arr.get(0));
+
+        MyArrayList list1 = new MyArrayList();
+        list1.add(4);
+        System.out.println(list1.get(0));
+
     }
 }
